2b3c8eb52e8322ba538da3b3b1643c40c8b8ec9d
Lotanna Anapugars <S2400236@ndonline.ac.uk>
2025-02-06 10:32:26 +0000

    Added validation and began implementation of custom borad dimension logic

--- Skeleton/Skeleton.py
+++ Skeleton/Skeleton.py
@@ -17,11 +17,16 @@ def GetRowColumn():
         except ValueError:
             print("Incorrect data type")
 
-
-    Column = int(input("Please enter column: "))
-    while (Column < 0 or Column > 9):
-        print("Out of bounds of the board, please enter again")
-        Column = int(input("Please enter column: "))
+    valid = False
+    while not valid:
+        try:
+            Column = int(input("Please enter column: "))
+            if (Column < 0 or Column > 9):
+                print("Out of bounds of the board, please enter again")
+            else: 
+                valid = True
+        except ValueError:
+            print("Incorrect data type")
 
     print()
     return Row, Column
@@ -48,10 +53,68 @@ def SetUpBoard():
         Board.append(BoardRow)
     return Board
 
+def GetDimensionChoice():
+    print("""Do you want to load the default game or a custom game?
+    \n1. Default
+2. Custom""")
+    print ()
+    validChoice = False
+    while not validChoice:
+        try:
+            choice = int(input("Please enter your choice: "))
+            if choice < 0 and choice > 3:
+                valid = True
+            else:
+                print("Please enter a valid choice")
+
+            if choice == 1:
+                SetUpBoard()
+            else:
+                SetupCustomBoard()
+
+        except ValueError:
+            print("Please enter a valid choice")
+    
+    print()
+    return choice
+
+def SetupCustomBoard():
+    Board = []
+    print("Specify the number of rows and columns below")
+    valid = False
+    while not valid:
+        try:
+            Rows = int(input("How many rows do you want? "))
+            if Rows > 0:
+                valid = True
+            else:
+                print("Enter a number greater than 0")
+        except ValueError:
+            print("Please enter a valid number")
+
+    valid = False
+    while not valid:
+        try:
+            Columns = int(input("How many columns do you want? "))
+            print()
+            if Columns > 0:
+                valid = True
+            else:
+                print("Enter a number greater than 0")
+        except ValueError:
+            print("Please enter a valid number")
+
+    for Row in range(Rows):
+        BoardRow = []
+        for Column in range(Columns):
+            BoardRow.append("-")
+        Board.append(BoardRow)
+    return Board
+
 
 def LoadGame(Filename, Board):
     BoardFile = open(Filename, "r")
-    for Row in range(10):
+    for Row in range():
         Line = BoardFile.readline()
         for Column in range(10):
             Board[Row][Column] = Line[Column]
@@ -141,7 +204,14 @@ def DisplayMenu():
 
 def GetMainMenuChoice():
     print("Please enter your choice: ", end="")
-    Choice = int(input())
+    choiceEntered = False
+    while not choiceEntered:
+        try:
+            Choice = int(input("Please enter your choice: "))
+            choiceEntered = True
+        except:
+            print("Try again...")
+    
     print()
     return Choice
 
@@ -161,11 +231,14 @@ if __name__ == "__main__":
     TRAININGGAME = "Training.txt"
     MenuOption = 0
     while not MenuOption == 9:
-        Board = SetUpBoard()
         Ships = [["Aircraft Carrier", 5], ["Battleship", 4], ["Submarine", 3], ["Destroyer", 3], ["Patrol Boat", 2]]
         DisplayMenu()
         MenuOption = GetMainMenuChoice()
         if MenuOption == 1:
+            if GetDimensionChoice() == 1:
+                PlaceRandomShips(Board, Ships)
+            else:
+                Board = SetupCustomBoard()
             PlaceRandomShips(Board, Ships)
             PlayGame(Board, Ships)
         if MenuOption == 2:
